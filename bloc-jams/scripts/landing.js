8aead5a1067714438a1d923b11d9c3e56d19fb8f
Justin <barreto5288@gmail.com>
2020-01-07 23:07:09 -0500

    Checkpoint 8 comnpleted

--- bloc-jams/scripts/landing.js
+++ bloc-jams/scripts/landing.js
@@ -2,13 +2,13 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
+    var revealPoint = function(i) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
     for (i = 0; i < points.length; i++) {
-        var revealPoint = function(i) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
         revealPoint(i);
     }
 };
